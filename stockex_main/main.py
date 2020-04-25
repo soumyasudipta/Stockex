0a2bc01b4fb5c14521cc03c61ccc182a9c2a216f
Soumya Sudipta Sarangi <soumya.sarangi.10@gmail.com>
2020-04-26 01:52:03 +0530

    Removed alphavantage

--- stockex_main/main.py
+++ stockex_main/main.py
@@ -2,14 +2,8 @@ import time
 import schedule 
 import requests
 import json
-from pymongo import MongoClient
 import pandas as pd
-
-# import alphavantage.daily as alphavantagedaily
-# import alphavantage.monthly as alphavantagemonthly
-# import alphavantage.weekly as alphavantageweekly
-# import alphavantage.conf as alphavantageconf
-# import alphavantage.daily_updated as daily_updated
+from pymongo import MongoClient
 
 # import yahoofinance.historical as historical
 import yahoofinance.yahoofinance_manager as yahoofinance_manager
@@ -17,11 +11,6 @@ import yahoofinance.yahoofinance_manager as yahoofinance_manager
 import mongodb.mongodatabase
 
 def main():
-    # alphavantagemonthly.jprint()
-    # alphavantagedaily.jprint()
-    # alphavantageweekly.jprint()
-    # daily_updated.jprint()
-    # historical.jprint()
     yahoofinance_manager.initiate()
     print('Inside main')
 
