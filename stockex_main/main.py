33ad3f5aaf3636fc7685bf9763802d3e71c6998c
Soumya Sudipta Sarangi <soumya.sarangi.10@gmail.com>
2020-05-14 17:22:59 +0530

    issues fixed

--- stockex_main/main.py
+++ stockex_main/main.py
@@ -28,42 +28,42 @@ def daily_insert_job():
     yahoofinance_manager.initiate(period_3mo, interval_1d)
     yahoofinance_manager.initiate(period_6mo, interval_1d)
     yahoofinance_manager.initiate(period_1y, interval_1d)
-    yahoofinance_manager.initiate(period_5y, interval_1d)
+    # yahoofinance_manager.initiate(period_5y, interval_1d)
     yahoofinance_manager.initiate(period_ytd, interval_1d)
-    yahoofinance_manager.initiate(period_max, interval_1d)
+    # yahoofinance_manager.initiate(period_max, interval_1d)
     print('Stopped the daily wise job')
 
 
 if __name__ == "__main__":
-    # os.environ['TZ'] = 'America/Detroit'
-    # time.tzset()
-    #
-    # now = datetime.datetime.now()
-    # current_time = int(now.strftime("%H")) * 60 + int(now.strftime("%M"))
-    # start = 9 * 60 + 25
-    # stop = 16 * 60 + 5
-    #
-    # if start < current_time < stop:
-    #     # After every min minute_job() is called.
-    #     schedule.every().minute.do(minute_job)
-    #     print(current_time)
-    #     print(start)
-    #     print(stop)
-    #
-    # # Task scheduling
-    # # # From Monday to Friday at 9:25am time daily_deletion() is called.
-    # schedule.every().monday.at("09:25").do(daily_delete_job)
-    # schedule.every().tuesday.at("09:25").do(daily_delete_job)
-    # schedule.every().wednesday.at("09:25").do(daily_delete_job)
-    # schedule.every().thursday.at("09:25").do(daily_delete_job)
-    # schedule.every().friday.at("09:25").do(daily_delete_job)
-    #
-    # # # From Monday to Friday at 12am or 00:00 time daily_job() is called.
-    # schedule.every().monday.at("00:00").do(daily_insert_job)
-    # schedule.every().tuesday.at("00:00").do(daily_insert_job)
-    # schedule.every().wednesday.at("00:00").do(daily_insert_job)
-    # schedule.every().thursday.at("00:00").do(daily_insert_job)
-    # schedule.every().friday.at("00:00").do(daily_insert_job)
+    os.environ['TZ'] = 'America/Detroit'
+    time.tzset()
+
+    now = datetime.datetime.now()
+    current_time = int(now.strftime("%H")) * 60 + int(now.strftime("%M"))
+    start = 9 * 60 + 25
+    stop = 16 * 60 + 5
+
+    if start < current_time < stop:
+        # After every min minute_job() is called.
+        schedule.every().minute.do(minute_job)
+        print(current_time)
+        print(start)
+        print(stop)
+
+    # Task scheduling
+    # # From Monday to Friday at 9:25am time daily_deletion() is called.
+    schedule.every().monday.at("09:25").do(daily_delete_job)
+    schedule.every().tuesday.at("09:25").do(daily_delete_job)
+    schedule.every().wednesday.at("09:25").do(daily_delete_job)
+    schedule.every().thursday.at("09:25").do(daily_delete_job)
+    schedule.every().friday.at("09:25").do(daily_delete_job)
+
+    # # From Monday to Friday at 12am or 00:00 time daily_job() is called.
+    schedule.every().monday.at("00:00").do(daily_insert_job)
+    schedule.every().tuesday.at("00:00").do(daily_insert_job)
+    schedule.every().wednesday.at("00:00").do(daily_insert_job)
+    schedule.every().thursday.at("00:00").do(daily_insert_job)
+    schedule.every().friday.at("00:00").do(daily_insert_job)
 
     daily_insert_job()
     minute_job()
