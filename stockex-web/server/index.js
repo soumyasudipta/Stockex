18db0b4a5b6bef5fe37893b9f5227e03e5a12a02
Soumya Sudipta Sarangi <soumyasudiptasarangi@gmail.com>
2020-05-18 22:13:38 +0530

    fgf

--- stockex-web/server/index.js
+++ stockex-web/server/index.js
@@ -12,6 +12,18 @@ const gets = require('./routes/api/gets');
 
 app.use('/api/gets',gets);
 
+
+// Handle Production
+
+if(process.env.NODE_ENV === 'production') {
+//        Static Folder
+    app.use(express.static(__dirname + '/public/'));
+
+//    Handle SPA
+    app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'))
+}
+
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
