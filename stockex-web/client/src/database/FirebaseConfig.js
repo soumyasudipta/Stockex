032b9d538b178cad6069ea19d640c61afcaab2a3
Soumya Sudipta Sarangi <soumyasudiptasarangi@gmail.com>
2020-05-28 23:38:20 +0530

    Removed api key

--- stockex-web/client/src/database/FirebaseConfig.js
+++ stockex-web/client/src/database/FirebaseConfig.js
@@ -3,14 +3,14 @@ import 'firebase/firestore'
 
 
 const config = {
-    apiKey: "AIzaSyAx74C9R7qNyrrPC8LxoZgBDvSlPG8T-OY",
-    authDomain: "stockex-soumyasudipta.firebaseapp.com",
-    databaseURL: "https://stockex-soumyasudipta.firebaseio.com",
-    projectId: "stockex-soumyasudipta",
-    storageBucket: "stockex-soumyasudipta.appspot.com",
-    messagingSenderId: "240783026657",
-    appId: "1:240783026657:web:12f342497d047e434f4aa0",
-    measurementId: "G-VBJZJ47HR3"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 }
 
 firebase.initializeApp(config)
